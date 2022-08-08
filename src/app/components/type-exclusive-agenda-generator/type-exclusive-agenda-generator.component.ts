1ac48f012faa4d1b0561b6a686c5f3567a82eb74
k-gan <moskala.k@gmail.com>
2022-08-08 11:56:33 +0200

    2 files modified

--- src/app/components/type-exclusive-agenda-generator/type-exclusive-agenda-generator.component.ts
+++ src/app/components/type-exclusive-agenda-generator/type-exclusive-agenda-generator.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { AgendaGenerationService } from '../../services/agenda-generation.service';
+import { AgendaSynchronizeService } from '../../services/agenda-synchronize.service';
+import { AtOfficeTimesProviderService } from '../../services/at-office-times-provider.service';
+import { AgendaModel } from '../agenda-generator/agenda-model';
 
 @Component({
   selector: 'app-type-exclusive-agenda-generator',
   templateUrl: './type-exclusive-agenda-generator.component.html',
-  styleUrls: ['./type-exclusive-agenda-generator.component.css']
+  styleUrls: ['./type-exclusive-agenda-generator.component.css'],
 })
-export class TypeExclusiveAgendaGeneratorComponent implements OnInit {
+export class TypeExclusiveAgendaGeneratorComponent {
+  Object = Object;
+  model: AgendaModel;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private agendaService: AgendaGenerationService,
+    private agendaSync: AgendaSynchronizeService,
+    atOfficeTimeService: AtOfficeTimesProviderService
+  ) {
+    const atOfficeTimes = atOfficeTimeService.generateAtOfficeSelection();
+    this.model = new AgendaModel(atOfficeTimes);
   }
 
+  onSubmit() {
+    const agenda = this.agendaService.generateAgenda(
+      this.model.upToDateAgendaInput
+    );
+    this.agendaSync.changeAgenda(agenda);
+  }
 }
