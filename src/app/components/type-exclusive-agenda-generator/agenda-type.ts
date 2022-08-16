f04db847080763d1ea2a0a04ff42798ec580ae31
k-gan <k-gan@github>
2022-08-16 11:44:22 +0200

    Converted Exclusive Agenda component to use enums.

---
+++ src/app/components/type-exclusive-agenda-generator/agenda-type.ts
@@ -0,0 +1,3 @@
+export enum AgendaType {
+    Default, Training, Tomek
+}
--- src/app/components/type-exclusive-agenda-generator/type-exclusive-agenda-generator.component.ts
+++ src/app/components/type-exclusive-agenda-generator/type-exclusive-agenda-generator.component.ts
@@ -3,6 +3,7 @@ import { AgendaGenerationService } from '../../services/agenda-generation.servic
 import { AgendaSynchronizeService } from '../../services/agenda-synchronize.service';
 import { AtOfficeTimesProviderService } from '../../services/at-office-times-provider.service';
 import { AgendaModel } from '../agenda-generator/agenda-model';
+import { AgendaType } from './agenda-type';
 
 @Component({
   selector: 'app-type-exclusive-agenda-generator',
@@ -12,8 +13,8 @@ import { AgendaModel } from '../agenda-generator/agenda-model';
 export class TypeExclusiveAgendaGeneratorComponent {
   Object = Object;
   model: AgendaModel;
-  agendaTypes: string[] = ['Default', 'Training', "Tomek's"];
-  agendaType: string = '';
+  readonly agendaTypes: AgendaType[];
+  agendaType: AgendaType;
 
   constructor(
     private agendaService: AgendaGenerationService,
@@ -22,6 +23,9 @@ export class TypeExclusiveAgendaGeneratorComponent {
   ) {
     const atOfficeTimes = atOfficeTimeService.generateAtOfficeSelection();
     this.model = new AgendaModel(atOfficeTimes);
+
+    const keys = Object.values(AgendaType).filter(k => typeof AgendaType[k as any] !== "number");
+    this.agendaTypes = keys.map((k) => AgendaType[k]);
     this.agendaType = this.agendaTypes[0];
   }
 
