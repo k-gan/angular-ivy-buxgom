91b8fba35c11577014def9d28f24957f09a9c7c5
k-gan <k-gan@github>
2022-08-17 10:14:54 +0200

    Deleted redundant settings.

--- src/app/settings.ts
+++ src/app/settings.ts
@@ -4,8 +4,4 @@ export class DayPlanSettings {
   readonly earliestAtOfficeTime: Time = { hours: 9, minutes: 0 };
   readonly latestAtOfficeTime: Time = { hours: 10, minutes: 30 };
   readonly atOfficeTimeIncrements: Time = { hours: 0, minutes: 15 };
-
-  readonly trainingWakeUpTime: Time = { hours: 5, minutes: 30 };
-  readonly trainingDriveToWorkDuration: Time = { hours: 0, minutes: 20 };
-  readonly trainingBathromTimeDuration: Time = { hours: 0, minutes: 20 };
 }
