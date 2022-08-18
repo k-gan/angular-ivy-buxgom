c6bb492d173ba7156fb1f08363efc126cbd6ddde
k-gan <k-gan@github>
2022-08-18 14:25:54 +0200

    Removed redundant comment

--- src/app/services/agenda/points/home-agenda-point.ts
+++ src/app/services/agenda/points/home-agenda-point.ts
@@ -1,8 +1,6 @@
 export enum HomeAgendaPoint {
-  DriveToOfficeFromHome = 'Drive Home -> Office',
-  Running = 'Running',
-  // TODO: Tomek's is still missing setting when to be at his - and then
-  // what it means for getting to the office
+  DriveToOfficeFromHome = "Drive Home -> Office",
+  Running = "Running",
   DriveToTomeksFromHome = "Drive Home -> Tomek's",
   AtTomeks = "At Tomek's",
   DriveToOfficeFromTomeks = "Drive Tomek's -> Office",
