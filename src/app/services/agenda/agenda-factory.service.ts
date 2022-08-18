c58397d2cd7db42e7d242d363cd9beb8aa92419c
k-gan <k-gan@github>
2022-08-18 14:25:14 +0200

    Removed missing agenda validator from being used.

--- src/app/services/agenda/agenda-factory.service.ts
+++ src/app/services/agenda/agenda-factory.service.ts
@@ -2,12 +2,10 @@ import { Injectable } from "@angular/core";
 import { Agenda } from "./agenda";
 import { AgendaType } from "./agenda-type";
 import { AgendaConfigurationService } from "./configuration/agenda-configuration.service";
-import { MissingAgendaValidatorService } from "./missing-agenda-validator.service";
 
 @Injectable({ providedIn: "root" })
 export class AgendaFactoryService {
   constructor(
-    private readonly missingAgendaValidator: MissingAgendaValidatorService,
     private readonly agendaConfigurationProvider: AgendaConfigurationService
   ) {}
 
