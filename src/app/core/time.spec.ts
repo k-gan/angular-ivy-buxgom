8bb51f1cd123322d71c789d8b87aa08e763b8e5d
k-gan <k-gan@github>
2022-08-26 12:32:17 +0200

    Tests: +select times, improved redability of time

--- src/app/core/time.spec.ts
+++ src/app/core/time.spec.ts
@@ -18,248 +18,259 @@ describe("Time", () => {
   });
 
   // Constructor tests
-  it("correctly assigns during instantiation", () => {
-    expect(time.hours).toEqual(defaultHours);
-    expect(time.minutes).toEqual(defaultMinutes);
+  describe("Constructor", () => {
+    it("correctly assigns during instantiation", () => {
+      expect(time.hours).toEqual(defaultHours);
+      expect(time.minutes).toEqual(defaultMinutes);
+    });
+
+    it("time throws error when hours lower than 0", () => {
+      expect(() => new Time(-1, 0)).toThrowError();
+    });
+
+    it("time throws error when hours larger than 23", () => {
+      expect(() => new Time(24, 0)).toThrowError();
+    });
+
+    it("time throws error when minutes lower than 0", () => {
+      expect(() => new Time(1, -1)).toThrowError();
+    });
+
+    it("time throws error when minutes larger than 59", () => {
+      expect(() => new Time(1, 60)).toThrowError();
+    });
   });
 
-  it("time throws error when hours lower than 0", () => {
-    expect(() => new Time(-1, 0)).toThrowError();
+  describe("Parse", () => {
+    it("parse returns correctly parsed for correct value", () => {
+      expect();
+    });
   });
 
-  it("time throws error when hours larger than 23", () => {
-    expect(() => new Time(24, 0)).toThrowError();
+  describe("Checksum", () => {
+    it("checksum equals to total minutes", () => {
+      const expChecksum = time.hours * 60 + time.minutes;
+
+      expect(time.checksum).toEqual(expChecksum);
+    });
   });
 
-  it("time throws error when minutes lower than 0", () => {
-    expect(() => new Time(1, -1)).toThrowError();
+  describe("isBefore", () => {
+    it("isBefore returns true when supplied is lower", () => {
+      expect(before.isBefore(after)).toBeTrue();
+    });
+
+    it("isBefore returns false when supplied is higher", () => {
+      expect(after.isBefore(before)).toBeFalse();
+    });
+
+    it("isBefore returns false when supplied is equal", () => {
+      expect(before.isBefore(equalsBefore)).toBeFalse();
+    });
   });
 
-  it("time throws error when minutes larger than 59", () => {
-    expect(() => new Time(1, 60)).toThrowError();
+  describe("isAfter", () => {
+    it("isAfter returns true when supplied is higher", () => {
+      expect(after.isAfter(before)).toBeTrue();
+    });
+
+    it("isAfter returns false when supplied is lower", () => {
+      expect(before.isAfter(after)).toBeFalse();
+    });
+
+    it("isAfter returns false when supplied is equal", () => {
+      expect(before.isAfter(equalsBefore)).toBeFalse();
+    });
   });
 
-  // parse()
-  it("parse returns correctly parsed for correct value", () => {
-    expect()
+  describe("isEqual", () => {
+    it("isEqual returns false when supplied is higher", () => {
+      expect(before.isEqual(after)).toBeFalse();
+    });
+
+    it("isEqual returns false when supplied is lower", () => {
+      expect(after.isEqual(before)).toBeFalse();
+    });
+
+    it("isEqual returns true when supplied is equal", () => {
+      expect(before.isEqual(equalsBefore)).toBeTrue();
+    });
   });
 
-  // Checksum
-  it("checksum equals to total minutes", () => {
-    const expChecksum = time.hours * 60 + time.minutes;
+  describe("add hours", () => {
+    it("add adds hours correctly when sum below 24", () => {
+      const expHours = 23;
+      const hoursToAdd = expHours - time.hours;
+      const timeToAdd = new Time(hoursToAdd, 0);
 
-    expect(time.checksum).toEqual(expChecksum);
+      const resultTime = time.add(timeToAdd);
+
+      expect(resultTime.hours).toBe(expHours);
+    });
+
+    it("adding 1 hour to 23 hours results in 0 hours", () => {
+      const baseTime = new Time(23, 0);
+      const addTime = new Time(1, 0);
+      const expHours = 0;
+
+      const resultTime = baseTime.add(addTime);
+
+      expect(resultTime.hours).toBe(expHours);
+    });
+
+    it("adding 2 hours to 23 hours results in 1 hours", () => {
+      const baseTime = new Time(23, 0);
+      const addTime = new Time(2, 0);
+      const expHours = 1;
+
+      const resultTime = baseTime.add(addTime);
+
+      expect(resultTime.hours).toBe(expHours);
+    });
+
+    it("adding 23 hours to 23 hours results in 22 hours", () => {
+      const baseTime = new Time(23, 0);
+      const addTime = new Time(23, 0);
+      const expHours = baseTime.hours + addTime.hours - 24;
+
+      const resultTime = baseTime.add(addTime);
+
+      expect(resultTime.hours).toBe(expHours);
+    });
   });
 
-  // isBefore()
-  it("isBefore returns true when supplied is lower", () => {
-    expect(before.isBefore(after)).toBeTrue();
+  describe("add minutes", () => {
+    it("add adds minutes correctly when sum below 60", () => {
+      const expMinutes = 59;
+      const minutesToAdd = expMinutes - time.minutes;
+      const timeToAdd = new Time(time.hours, minutesToAdd);
+
+      const resultTime = time.add(timeToAdd);
+
+      expect(resultTime.minutes).toBe(expMinutes);
+    });
+
+    it("adding 1 minute to 59 minutes results in adding 1 to hours and setting minutes to 0", () => {
+      const baseTime = new Time(1, 59);
+      const addTime = new Time(0, 1);
+      const expTime = new Time(baseTime.hours + 1, 0);
+
+      const resultTime = baseTime.add(addTime);
+
+      expect(resultTime.hours).toBe(expTime.hours);
+      expect(resultTime.minutes).toBe(expTime.minutes);
+    });
+
+    it("adding 2 minutes to 59 minutes results in 1 minute", () => {
+      const baseTime = new Time(1, 59);
+      const addTime = new Time(0, 2);
+      const expTime = new Time(baseTime.hours + 1, 1);
+
+      const resultTime = baseTime.add(addTime);
+
+      expect(resultTime.hours).toBe(expTime.hours);
+      expect(resultTime.minutes).toBe(expTime.minutes);
+    });
+
+    it("adding 59 minutes to 59 minutes results in 58 minutes", () => {
+      const baseTime = new Time(1, 59);
+      const addTime = new Time(0, 59);
+      const expTime = new Time(baseTime.hours + 1, 58);
+
+      const resultTime = baseTime.add(addTime);
+
+      expect(resultTime.hours).toBe(expTime.hours);
+      expect(resultTime.minutes).toBe(expTime.minutes);
+    });
   });
 
-  it("isBefore returns false when supplied is higher", () => {
-    expect(after.isBefore(before)).toBeFalse();
+  describe("subtract hours", () => {
+    it("subtract subtracts hours correctly when sum equal or higher than 0", () => {
+      const expHours = 0;
+      const hoursToSubtract = expHours + time.hours;
+      const timeToAdd = new Time(hoursToSubtract, 0);
+
+      const resultTime = time.subtract(timeToAdd);
+
+      expect(resultTime.hours).toBe(expHours);
+    });
+
+    it("subracting 1 hour from 0 hours results in 23 hours", () => {
+      const baseTime = new Time(0, 0);
+      const subtractTime = new Time(1, 0);
+      const expHours = 23;
+
+      const resultTime = baseTime.subtract(subtractTime);
+
+      expect(resultTime.hours).toBe(expHours);
+    });
+
+    it("subtracting 2 hours from 0 hours results in 22 hours", () => {
+      const baseTime = new Time(0, 0);
+      const subtractTime = new Time(2, 0);
+      const expHours = 22;
+
+      const resultTime = baseTime.subtract(subtractTime);
+
+      expect(resultTime.hours).toBe(expHours);
+    });
+
+    it("subtracting 23 hours from 23 hours results in 22 hours", () => {
+      const baseTime = new Time(0, 0);
+      const subtractTime = new Time(2, 0);
+      const expHours = 22;
+
+      const resultTime = baseTime.subtract(subtractTime);
+
+      expect(resultTime.hours).toBe(expHours);
+    });
   });
 
-  it("isBefore returns false when supplied is equal", () => {
-    expect(before.isBefore(equalsBefore)).toBeFalse();
-  });
+  describe("subtract minutes", () => {
+    it("subtract subtracts minutes correctly when sum equal or higher than 0", () => {
+      const expMinutes = 0;
+      const minutesToSubtract = expMinutes + time.minutes;
+      const timeToSubtract = new Time(0, minutesToSubtract);
 
-  // isAfter()
-  it("isAfter returns true when supplied is higher", () => {
-    expect(after.isAfter(before)).toBeTrue();
-  });
+      const resultTime = time.subtract(timeToSubtract);
 
-  it("isAfter returns false when supplied is lower", () => {
-    expect(before.isAfter(after)).toBeFalse();
-  });
+      expect(resultTime.minutes).toBe(expMinutes);
+    });
 
-  it("isAfter returns false when supplied is equal", () => {
-    expect(before.isAfter(equalsBefore)).toBeFalse();
-  });
+    it("subtracting 1 minute from 0:0 minutes results in 23:59", () => {
+      const baseTime = new Time(0, 0);
+      const subtractTime = new Time(0, 1);
+      const expHours = 23;
+      const expMinutes = 59;
 
-  // isEqual()
-  it("isEqual returns false when supplied is higher", () => {
-    expect(before.isEqual(after)).toBeFalse();
-  });
+      const resultTime = baseTime.subtract(subtractTime);
 
-  it("isEqual returns false when supplied is lower", () => {
-    expect(after.isEqual(before)).toBeFalse();
-  });
+      expect(resultTime.hours).toBe(expHours);
+      expect(resultTime.minutes).toBe(expMinutes);
+    });
 
-  it("isEqual returns true when supplied is equal", () => {
-    expect(before.isEqual(equalsBefore)).toBeTrue();
-  });
+    it("subtracting 2 minutes from 0:0 results in 23:58", () => {
+      const baseTime = new Time(0, 0);
+      const subtractTime = new Time(0, 2);
+      const expHours = 23;
+      const expMinutes = 58;
 
-  // add() hours
-  it("add adds hours correctly when sum below 24", () => {
-    const expHours = 23;
-    const hoursToAdd = expHours - time.hours;
-    const timeToAdd = new Time(hoursToAdd, 0);
+      const resultTime = baseTime.subtract(subtractTime);
 
-    const resultTime = time.add(timeToAdd);
+      expect(resultTime.hours).toBe(expHours);
+      expect(resultTime.minutes).toBe(expMinutes);
+    });
 
-    expect(resultTime.hours).toBe(expHours);
-  });
+    it("subtracting 59 minutes from 59 minutes results in 0 minutes and the same hours", () => {
+      const baseTime = new Time(0, 59);
+      const subtractTime = new Time(0, 59);
+      const expHours = 0;
+      const expMinutes = 0;
 
-  it("adding 1 hour to 23 hours results in 0 hours", () => {
-    const baseTime = new Time(23, 0);
-    const addTime = new Time(1, 0);
-    const expHours = 0;
+      const resultTime = baseTime.subtract(subtractTime);
 
-    const resultTime = baseTime.add(addTime);
-
-    expect(resultTime.hours).toBe(expHours);
-  });
-
-  it("adding 2 hours to 23 hours results in 1 hours", () => {
-    const baseTime = new Time(23, 0);
-    const addTime = new Time(2, 0);
-    const expHours = 1;
-
-    const resultTime = baseTime.add(addTime);
-
-    expect(resultTime.hours).toBe(expHours);
-  });
-
-  it("adding 23 hours to 23 hours results in 22 hours", () => {
-    const baseTime = new Time(23, 0);
-    const addTime = new Time(23, 0);
-    const expHours = baseTime.hours + addTime.hours - 24;
-
-    const resultTime = baseTime.add(addTime);
-
-    expect(resultTime.hours).toBe(expHours);
-  });
-
-  // add() minutes
-  it("add adds minutes correctly when sum below 60", () => {
-    const expMinutes = 59;
-    const minutesToAdd = expMinutes - time.minutes;
-    const timeToAdd = new Time(time.hours, minutesToAdd);
-
-    const resultTime = time.add(timeToAdd);
-
-    expect(resultTime.minutes).toBe(expMinutes);
-  });
-
-  it("adding 1 minute to 59 minutes results in adding 1 to hours and setting minutes to 0", () => {
-    const baseTime = new Time(1, 59);
-    const addTime = new Time(0, 1);
-    const expTime = new Time(baseTime.hours + 1, 0);
-
-    const resultTime = baseTime.add(addTime);
-
-    expect(resultTime.hours).toBe(expTime.hours);
-    expect(resultTime.minutes).toBe(expTime.minutes);
-  });
-
-  it("adding 2 minutes to 59 minutes results in 1 minute", () => {
-    const baseTime = new Time(1, 59);
-    const addTime = new Time(0, 2);
-    const expTime = new Time(baseTime.hours + 1, 1);
-
-    const resultTime = baseTime.add(addTime);
-
-    expect(resultTime.hours).toBe(expTime.hours);
-    expect(resultTime.minutes).toBe(expTime.minutes);
-  });
-
-  it("adding 59 minutes to 59 minutes results in 58 minutes", () => {
-    const baseTime = new Time(1, 59);
-    const addTime = new Time(0, 59);
-    const expTime = new Time(baseTime.hours + 1, 58);
-
-    const resultTime = baseTime.add(addTime);
-
-    expect(resultTime.hours).toBe(expTime.hours);
-    expect(resultTime.minutes).toBe(expTime.minutes);
-  });
-
-  // subtract() hours
-  it("subtract subtracts hours correctly when sum equal or higher than 0", () => {
-    const expHours = 0;
-    const hoursToSubtract = expHours + time.hours;
-    const timeToAdd = new Time(hoursToSubtract, 0);
-
-    const resultTime = time.subtract(timeToAdd);
-
-    expect(resultTime.hours).toBe(expHours);
-  });
-
-  it("subracting 1 hour from 0 hours results in 23 hours", () => {
-    const baseTime = new Time(0, 0);
-    const subtractTime = new Time(1, 0);
-    const expHours = 23;
-
-    const resultTime = baseTime.subtract(subtractTime);
-
-    expect(resultTime.hours).toBe(expHours);
-  });
-
-  it("subtracting 2 hours from 0 hours results in 22 hours", () => {
-    const baseTime = new Time(0, 0);
-    const subtractTime = new Time(2, 0);
-    const expHours = 22;
-
-    const resultTime = baseTime.subtract(subtractTime);
-
-    expect(resultTime.hours).toBe(expHours);
-  });
-
-  it("subtracting 23 hours from 23 hours results in 22 hours", () => {
-    const baseTime = new Time(0, 0);
-    const subtractTime = new Time(2, 0);
-    const expHours = 22;
-
-    const resultTime = baseTime.subtract(subtractTime);
-
-    expect(resultTime.hours).toBe(expHours);
-  });
-
-  // subtract() minutes
-  it("subtract subtracts minutes correctly when sum equal or higher than 0", () => {
-    const expMinutes = 0;
-    const minutesToSubtract = expMinutes + time.minutes;
-    const timeToSubtract = new Time(0, minutesToSubtract);
-
-    const resultTime = time.subtract(timeToSubtract);
-
-    expect(resultTime.minutes).toBe(expMinutes);
-  });
-
-  it("subtracting 1 minute from 0:0 minutes results in 23:59", () => {
-    const baseTime = new Time(0, 0);
-    const subtractTime = new Time(0, 1);
-    const expHours = 23;
-    const expMinutes = 59;
-
-    const resultTime = baseTime.subtract(subtractTime);
-
-    expect(resultTime.hours).toBe(expHours);
-    expect(resultTime.minutes).toBe(expMinutes);
-  });
-
-  it("subtracting 2 minutes from 0:0 results in 23:58", () => {
-    const baseTime = new Time(0, 0);
-    const subtractTime = new Time(0, 2);
-    const expHours = 23;
-    const expMinutes = 58;
-
-    const resultTime = baseTime.subtract(subtractTime);
-
-    expect(resultTime.hours).toBe(expHours);
-    expect(resultTime.minutes).toBe(expMinutes);
-  });
-
-  it("subtracting 59 minutes from 59 minutes results in 0 minutes and the same hours", () => {
-    const baseTime = new Time(0, 59);
-    const subtractTime = new Time(0, 59);
-    const expHours = 0;
-    const expMinutes = 0;
-
-    const resultTime = baseTime.subtract(subtractTime);
-
-    expect(resultTime.hours).toBe(expHours);
-    expect(resultTime.minutes).toBe(expMinutes);
+      expect(resultTime.hours).toBe(expHours);
+      expect(resultTime.minutes).toBe(expMinutes);
+    });
   });
 });
--- src/app/services/select-times.service.spec.ts
+++ src/app/services/select-times.service.spec.ts
@@ -1,8 +1,84 @@
+import { AgendaPointSettings } from "../agenda-point-settings";
+import { Time } from "../core/time";
 import { DayPlanSettings } from "../settings.service";
+import { SelectTimesService } from "./select-times.service";
 
 describe("SelectTimesService", () => {
-  let settings: DayPlanSettings;
-  beforeEach(() => {
-    settings = new DayPlanSettings();
-  });
+  const increments = new Time(0, 10);
+  const bottomBoundary = new Time(9, 0);
+  const upperBoundary = new Time(10, 0);
+  const apSetting = new AgendaPointSettings(bottomBoundary, upperBoundary);
+  const testSettings = [
+    { method: "generateAtOfficeSelection", property: "atOffice" },
+    { method: "generateAtTomeksSelection", property: "atTomeks" },
+    { method: "generateWorkoutSelection", property: "workout" },
+  ];
+
+  for (let testSetting of testSettings) {
+    describe(`Test ${testSetting.property}`, () => {
+      const expectedSelection = generateSelection(apSetting, increments);
+      const settings = createSettings(
+        testSetting.property,
+        apSetting,
+        increments
+      );
+      const service = new SelectTimesService(settings);
+      const actualSelection = service[testSetting.method]();
+
+      it(`${testSetting.method} bottom boundary is set correctly`, () => {
+        expect(actualSelection[0].checksum).toEqual(
+          expectedSelection[0].checksum
+        );
+      });
+
+      it(`${testSetting.method} to have correct number of items`, () => {
+        expect(actualSelection.length).toEqual(expectedSelection.length);
+      });
+
+      it(`${testSetting.method} upper boundary is set correctly`, () => {
+        const last = actualSelection.length - 1;
+        expect(actualSelection[last].checksum).toEqual(
+          expectedSelection[last].checksum
+        );
+      });
+
+      it(`${testSetting.method} selection is generated correctly`, () => {
+        for (const [idx, expSel] of expectedSelection.entries()) {
+          const actSel = actualSelection[idx];
+          expect(expSel.checksum).toBe(actSel.checksum);
+        }
+      });
+    });
+  }
+
+  function createSettings(
+    property: string,
+    atOffice: AgendaPointSettings,
+    increments: Time
+  ): DayPlanSettings {
+    let settings = jasmine.createSpyObj("DayPlanSettings", [], [property]);
+    Object.defineProperty(settings, property, {
+      value: atOffice,
+    });
+    Object.defineProperty(settings, "selectionTimesIncrements", {
+      value: increments,
+    });
+
+    return settings;
+  }
+
+  function generateSelection(
+    setting: AgendaPointSettings,
+    increments: Time
+  ): Time[] {
+    const times = new Array<Time>();
+
+    let curTime = setting.earliest;
+    while (curTime.isBefore(setting.latest.add(increments))) {
+      times.push(curTime);
+      curTime = curTime.add(increments);
+    }
+
+    return times;
+  }
 });
